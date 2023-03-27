a70e41dfb734d3e0bd7ca72de2a8536aecb4b566
AidenL <115218707+JihyeongL@users.noreply.github.com>
2023-03-27 23:15:14 +0900

    게시판 layout 작업 및 css 적용중

--- src/pages/board/Board.js
+++ src/pages/board/Board.js
@@ -1,102 +1,102 @@
-import boardcss from "./Board.module.css";
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+// import boardcss from "./Board.module.css";
+// import { useEffect, useState } from "react";
+// import { useNavigate } from "react-router-dom";
+// import { useSelector, useDispatch } from "react-redux";
 
 
-function Board (){
+// function Board (){
 
-const navigate = useNavigate();
-const dispatch = useDispatch();
+// const navigate = useNavigate();
+// const dispatch = useDispatch();
 
-const [currentPage, setCurrentPage] = useState(1);
+// const [currentPage, setCurrentPage] = useState(1);
 
-const pageNumber = [];
-if(pageInfo){
-    for(let i = 1; i <= pageInfo.endPage; i++){
-        pageNumber.push(i);
-    }
-}
+// const pageNumber = [];
+// if(pageInfo){
+//     for(let i = 1; i <= pageInfo.endPage; i++){
+//         pageNumber.push(i);
+//     }
+// }
 
-useEffect (() =>{
-    dispatch(callPostingListsAPI({
-        currentPage : currentPage
-    }));
-}, [currentPage]
-)
+// useEffect (() =>{
+//     dispatch(callPostingListsAPI({
+//         currentPage : currentPage
+//     }));
+// }, [currentPage]
+// )
 
 
-    return(
-        <>
-        <div className={boardcss.title}>게시판</div>
-        <br/>
-        <br/>
-            <div>
-                <table>
-                    <colgroup>
-                        <col width="10%"/>
-                        <col width="50%"/>
-                        <col width="20%"/>
-                        <col width="20%"/>
-                    </colgroup>
-                    <thead>
-                        <tr className={boardcss.tableheader}>
-                            <th>No.</th>
-                            <th>제 목</th>
-                            <th>등록자</th>
-                            <th>날 짜</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td/>
-                            <td/>
-                            <td/>
-                            <td/>
-                        </tr>
-                        <tr>
-                            <td/>
-                            <td/>
-                            <td/>
-                            <td/>
-                        </tr>
-                    </tbody>
-                </table>
-                <div>
-                <button className={boardcss.btnstyle}>
-                    등 록
-                </button>
-                </div>
-            </div>
+//     return(
+//         <>
+//         <div className={boardcss.title}>게시판</div>
+//         <br/>
+//         <br/>
+//             <div>
+//                 <table>
+//                     <colgroup>
+//                         <col width="10%"/>
+//                         <col width="50%"/>
+//                         <col width="20%"/>
+//                         <col width="20%"/>
+//                     </colgroup>
+//                     <thead>
+//                         <tr className={boardcss.tableheader}>
+//                             <th>No.</th>
+//                             <th>제 목</th>
+//                             <th>등록자</th>
+//                             <th>날 짜</th>
+//                         </tr>
+//                     </thead>
+//                     <tbody>
+//                         <tr>
+//                             <td/>
+//                             <td/>
+//                             <td/>
+//                             <td/>
+//                         </tr>
+//                         <tr>
+//                             <td/>
+//                             <td/>
+//                             <td/>
+//                             <td/>
+//                         </tr>
+//                     </tbody>
+//                 </table>
+//                 <div>
+//                 <button className={boardcss.btnstyle}>
+//                     등 록
+//                 </button>
+//                 </div>
+//             </div>
            
-            <br/>
-            <div>
-                <div className={boardcss.pagingbtn}>
-                    {
-                        <button style={{color: '#E52E2E', border:'none', fontWeight:'bold', fontSize:'15px'}}>
-                            &lt;&lt;
-                        </button>
-                    }
-                    {
-                        pageNumber.map((num) => (
-                            <li style={{listStyle: 'none', display:'inline'}} key={num} onClick={() => setCurrentPage(num)}>
-                                <button style={currentPage === num ? {backgroundColor : 'gray'} : null}>
-                                    {num}
-                                </button>
-                            </li>
-                        ))
-                    }
-                    {
-                        <button style={{color: '#E52E2E', border:'none', fontWeight:'bold', fontSize:'15px'}}>
-                            &gt;&gt;
-                        </button>
+//             <br/>
+//             <div>
+//                 <div className={boardcss.pagingbtn}>
+//                     {
+//                         <button style={{color: '#E52E2E', border:'none', fontWeight:'bold', fontSize:'15px'}}>
+//                             &lt;&lt;
+//                         </button>
+//                     }
+//                     {
+//                         pageNumber.map((num) => (
+//                             <li style={{listStyle: 'none', display:'inline'}} key={num} onClick={() => setCurrentPage(num)}>
+//                                 <button style={currentPage === num ? {backgroundColor : 'gray'} : null}>
+//                                     {num}
+//                                 </button>
+//                             </li>
+//                         ))
+//                     }
+//                     {
+//                         <button style={{color: '#E52E2E', border:'none', fontWeight:'bold', fontSize:'15px'}}>
+//                             &gt;&gt;
+//                         </button>
                         
-                    }
+//                     }
                     
                    
-                </div>
+//                 </div>
       
-        </div>
-        </>
-    )
-} export default Board;
+//         </div>
+//         </>
+//     )
+// } export default Board;
