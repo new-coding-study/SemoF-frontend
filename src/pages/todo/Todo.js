fcbc196d1a37e9930ee5ae0876d45e2ef4756726
hijee-ce <wlgml6925@gmail.com>
2023-03-27 15:57:52 +0900

    할 일 레이아웃 완료 (백단 연결 전)

--- src/pages/todo/Todo.js
+++ src/pages/todo/Todo.js
@@ -23,11 +23,46 @@ function Todo() {
           <Category /> {/* 리스트로 받아서 for문으로 출력해야함 */}
         </div>
         <div className={TodoCSS.content}>
-          <div className={TodoCSS.addTodo}> </div>
-          <div className={TodoCSS.graph}></div>
+          <div className={TodoCSS.addWrapper}>
+            <div className={TodoCSS.addInputWrapper}>
+              <div></div>
+              <input
+                type="text"
+                placeholder="새로운 할 일을 입력하세요"
+                // value={search}
+                // onKeyUp={onEnterkeyHandler}
+                // onChange={onSearchChangeHandler}
+              />
+              <img src={"/images/star_gray.png"} alt="이미지확인!"></img>
+              <img src={"/images/calendar_gray.png"} alt="이미지확인!"></img>
+            </div>
+            <button
+            // onClick={onClickLoginHandler}
+            >
+              할 일 추가
+            </button>
+          </div>
+          <div className={TodoCSS.graphWrapper}>
+            <div className={TodoCSS.graphTitle}>
+              <div> 오늘의 달성률 </div>
+              <span> 4 </span>
+              <span> / 6</span>
+            </div>
+            <div className={TodoCSS.graph}>
+              <div> </div>
+              <span> 66%</span>
+            </div>
+          </div>
           <div className={TodoCSS.todoList}>
-            <Today />
-            <Intended />
+            <div className={TodoCSS.today}>
+              <h2> 오늘의 할 일 </h2>
+              <Today />
+            </div>
+
+            <div className={TodoCSS.intended}>
+              <h2> 예정된 할 일 </h2>
+              <Intended />
+            </div>
           </div>
         </div>
       </div>
