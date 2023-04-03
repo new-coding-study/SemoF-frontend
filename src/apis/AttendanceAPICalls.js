9a5aa703c4b70432a05162e853d754960466ad4a
sik <seonsik7@gmail.com>
2023-04-03 20:49:52 +0900

    23-04-03(월) / 근태 관리 DB연결, 출퇴근 처리 - 사원정보 DB 연결 - 현재 시간 불러오기 - 출근 클릭 시 스탑워치 작동(퇴근 클릭 시 정지), 출퇴근에 따른 버튼 CSS 표현 변경 - 출근 기록 그래프화 예정

--- src/apis/AttendanceAPICalls.js
+++ src/apis/AttendanceAPICalls.js
@@ -1,23 +1,18 @@
 
 import { 
-    GET_STICKER,
-    GET_STICKERS,
-    // GET_STICKERS_LEGEND,
-    // GET_STICKERS_FIRST,
-    // GET_STICKERS_SECOND,
-    POST_STICKER,
-    PUT_STICKER,
-    DELETE_STICKER
+    GET_STATUS,
+    GET_HISTORIES,
+    GET_ANNUAL,
+    PUT_STATUS
 } from '../modules/AttendanceModule.js';
 
 // 근태 검색
-export const callSearchAttendanceAPI = ({searchValue, condition}) => {
+export const callAttendanceDetailAPI = ({empNo}) => {
     console.log('[AttendanceAPICalls] callSearchAttendanceAPI Call');
 
-    console.log(searchValue)
-    console.log(condition)
+    console.log(empNo)
 
-    const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8090/api/v1/attendance/search?condition=${condition}&searchValue=${searchValue}`;
+    const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8090/attendance/status/${empNo}`;
 
     console.log(requestURL)
     
@@ -34,181 +29,16 @@ export const callSearchAttendanceAPI = ({searchValue, condition}) => {
 
         console.log('[AttendanceAPICalls] callSearchAttendanceAPI RESULT : ', result);
 
-        dispatch({ type: GET_STICKERS,  payload: result.data });
+        dispatch({ type: GET_STATUS,  payload: result.data });
         
     };    
 };
 
-// 근태 전체조회
-export const callAttendanceListAPI = ({currentPage}) => {
-    
-    let requestURL;
-
-    if(currentPage !== undefined || currentPage !== null){
-        requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8090/api/v1/attendance?offset=${currentPage}`;
-    }else {
-        requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8090/api/v1/attendance`;
-    }
-    
-    console.log('[AttendanceAPICalls] requestURL : ', requestURL);
-
-    return async (dispatch, getState) => {
-
-        const result = await fetch(requestURL, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "*/*"                
-            }
-        })
-        .then(response => response.json());
-        if(result.status === 200){
-            console.log('[AttendanceAPICalls] callAttendanceAPI RESULT : ', result);
-            dispatch({ type: GET_STICKERS,  payload: result.data });
-        }
-        
-    };
-}
-
-// 근태 상세조회
-export const callAttendanceDetailAPI = ({attendanceCode}) => {
-    const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8090/api/v1/attendance/${attendanceCode}`;
-
-    return async (dispatch, getState) => {
-
-
-        const result = await fetch(requestURL, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "*/*"
-            }
-        })
-        .then(response => response.json());
-
-        console.log('[AttendanceAPICalls] callAttendanceDetailAPI RESULT : ', result);
-        if(result.status === 200){
-            console.log('[AttendanceAPICalls] callAttendanceDetailAPI SUCCESS');
-            dispatch({ type: GET_STICKER,  payload: result.data });
-        }
-        
-        
-    };
-}
-
-// 관리자용 근태 리스트 조회
-export const callAttendanceListForAdminAPI = ({currentPage}) => {
-    let requestURL;
-
-    if(currentPage !== undefined || currentPage !== null){
-        requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8090/api/v1/attendance-management?offset=${currentPage}`;
-    }else {
-        requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8090/api/v1/attendance-management`;
-    }
-    
-    console.log('[AttendanceAPICalls] requestURL : ', requestURL);
-
-    return async (dispatch, getState) => {
-
-        const result = await fetch(requestURL, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "*/*",
-                "Authorization": "Bearer " + window.localStorage.getItem("accessToken")
-            }
-        })
-        .then(response => response.json());
-        if(result.status === 200){
-            console.log('[AttendanceAPICalls] callAttendanceListForAdminAPI RESULT : ', result);
-            dispatch({ type: GET_STICKERS,  payload: result.data });
-        }
-        
-    };
-}
-
-// 관리자용 근태 상세조회
-export const callAttendanceDetailForAdminAPI = ({attendanceCode}) => {
-    const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8090/api/v1/attendance-management/${attendanceCode}`;
-
-    return async (dispatch, getState) => {
-
-
-        const result = await fetch(requestURL, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "*/*",
-                "Authorization": "Bearer " + window.localStorage.getItem("accessToken")
-            }
-        })
-        .then(response => response.json());
-
-        console.log('[AttendanceAPICalls] callAttendanceDetailAPI RESULT : ', result);
-        if(result.status === 200){
-            console.log('[AttendanceAPICalls] callAttendanceDetailAPI SUCCESS');
-            dispatch({ type: GET_STICKER,  payload: result.data });
-        }
-    };
-}
-
-// 관리자용 근태 등록
-export const callAttendanceRegistAPI = ({form}) => {
-    console.log('[AttendanceAPICalls] callAttendanceRegistAPI Call');
-
-    const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8090/api/v1/attendance-management`;
-
-    return async (dispatch, getState) => {
-
-        const result = await fetch(requestURL, {
-            method: "POST",
-            headers: {
-                "Accept": "*/*",
-                "Authorization": "Bearer " + window.localStorage.getItem("accessToken")
-            },
-            body: form
-        })
-        .then(response => response.json());
-
-        console.log('[AttendanceAPICalls] callAttendanceRegistAPI RESULT : ', result);
-
-        dispatch({ type: POST_STICKER,  payload: result });
-        
-    };    
-}
-
-// 관리자용 근태 삭제
-export const callAttendanceDeleteAPI = ({attendanceCode}) => {
-    console.log('[AttendanceAPICalls] callAttendanceDeleteAPI Call');
-
-    const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8090/api/v1/attendance-management/${attendanceCode}`;
-
-    return async (dispatch, getState) => {
-
-        const result = await fetch(requestURL, {
-            method: "DELETE",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "*/*",
-                "Authorization": "Bearer " + window.localStorage.getItem("accessToken")
-            }
-        })
-        .then(response => response.json());
-
-        console.log('[AttendanceAPICalls] callAttendanceDeleteAPI RESULT : ', result);
-
-        dispatch({ type: DELETE_STICKER,  payload: result });
-        
-    };    
-}
-
-// 관리자용 근태 수정
-export const callAttendanceUpdateAPI = ({form}) => {
+// 근태 상태 수정
+export const callAttendanceUpdateAPI = ({empNo}) => {
     console.log('[AttendanceAPICalls] callAttendanceUpdateAPI Call');
-    
-    const attendanceCode = form.get('attendanceCode')     // # formData에 있는 값을 가져올땐 .get메소드로 가져옴
 
-    const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8090/api/v1/attendance-management/${attendanceCode}`;
+    const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8090/attendance/status/${empNo}`;
 
     return async (dispatch, getState) => {
 
@@ -216,59 +46,248 @@ export const callAttendanceUpdateAPI = ({form}) => {
             method: "PUT",
             headers: {
                 "Accept": "*/*",
-                "Authorization": "Bearer " + window.localStorage.getItem("accessToken")
-            },
-            body: form
+                //"Authorization": "Bearer " + window.localStorage.getItem("accessToken")
+            }
         })
         .then(response => response.json());
 
         console.log('[AttendanceAPICalls] callAttendanceUpdateAPI RESULT : ', result);
 
-        dispatch({ type: PUT_STICKER,  payload: result });
+        dispatch({ type: PUT_STATUS,  payload: result });
         
     };    
 }
 
-//카테고리별 근태 목록 조회
-export const callAttendanceListAboutCategoryAPI = ({categoryCode}) => {
-    const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8090/api/v1/attendance/categories/${categoryCode}`;
+// 근태 전체조회
+// export const callAttendanceListAPI = ({currentPage}) => {
+    
+//     let requestURL;
 
-    return async (dispatch, getState) => {
+//     if(currentPage !== undefined || currentPage !== null){
+//         requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8090/api/v1/attendance?offset=${currentPage}`;
+//     }else {
+//         requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8090/api/v1/attendance`;
+//     }
+    
+//     console.log('[AttendanceAPICalls] requestURL : ', requestURL);
 
-        const result = await fetch(requestURL, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "*/*"                
-            }
-        })
-        .then(response => response.json());
-        if(result.status === 200){
-            console.log('[AttendanceAPICalls] callAttendanceListAboutCategoryAPI RESULT : ', result);
-            dispatch({ type: GET_STICKERS,  payload: result.data });
-        }
+//     return async (dispatch, getState) => {
+
+//         const result = await fetch(requestURL, {
+//             method: "GET",
+//             headers: {
+//                 "Content-Type": "application/json",
+//                 "Accept": "*/*"                
+//             }
+//         })
+//         .then(response => response.json());
+//         if(result.status === 200){
+//             console.log('[AttendanceAPICalls] callAttendanceAPI RESULT : ', result);
+//             dispatch({ type: GET_STICKERS,  payload: result.data });
+//         }
         
-    };
-}
+//     };
+// }
 
-//타입별 근태 목록 조회
-export const callAttendanceListAboutTypeAPI = ({typeCode}) => {
-    const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8090/api/v1/attendance/types/${typeCode}`;
+// // 근태 상세조회
+// export const callAttendanceDetailAPI = ({attendanceCode}) => {
+//     const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8090/api/v1/attendance/${attendanceCode}`;
 
-    return async (dispatch, getState) => {
+//     return async (dispatch, getState) => {
 
-        const result = await fetch(requestURL, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "*/*"                
-            }
-        })
-        .then(response => response.json());
-        if(result.status === 200){
-            console.log('[AttendanceAPICalls] callAttendanceListAboutTypeAPI RESULT : ', result);
-            dispatch({ type: GET_STICKERS,  payload: result.data });
-        }
+
+//         const result = await fetch(requestURL, {
+//             method: "GET",
+//             headers: {
+//                 "Content-Type": "application/json",
+//                 "Accept": "*/*"
+//             }
+//         })
+//         .then(response => response.json());
+
+//         console.log('[AttendanceAPICalls] callAttendanceDetailAPI RESULT : ', result);
+//         if(result.status === 200){
+//             console.log('[AttendanceAPICalls] callAttendanceDetailAPI SUCCESS');
+//             dispatch({ type: GET_STICKER,  payload: result.data });
+//         }
         
-    };
-}
+        
+//     };
+// }
+
+// // 관리자용 근태 리스트 조회
+// export const callAttendanceListForAdminAPI = ({currentPage}) => {
+//     let requestURL;
+
+//     if(currentPage !== undefined || currentPage !== null){
+//         requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8090/api/v1/attendance-management?offset=${currentPage}`;
+//     }else {
+//         requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8090/api/v1/attendance-management`;
+//     }
+    
+//     console.log('[AttendanceAPICalls] requestURL : ', requestURL);
+
+//     return async (dispatch, getState) => {
+
+//         const result = await fetch(requestURL, {
+//             method: "GET",
+//             headers: {
+//                 "Content-Type": "application/json",
+//                 "Accept": "*/*",
+//                 "Authorization": "Bearer " + window.localStorage.getItem("accessToken")
+//             }
+//         })
+//         .then(response => response.json());
+//         if(result.status === 200){
+//             console.log('[AttendanceAPICalls] callAttendanceListForAdminAPI RESULT : ', result);
+//             dispatch({ type: GET_STICKERS,  payload: result.data });
+//         }
+        
+//     };
+// }
+
+// // 관리자용 근태 상세조회
+// export const callAttendanceDetailForAdminAPI = ({attendanceCode}) => {
+//     const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8090/api/v1/attendance-management/${attendanceCode}`;
+
+//     return async (dispatch, getState) => {
+
+
+//         const result = await fetch(requestURL, {
+//             method: "GET",
+//             headers: {
+//                 "Content-Type": "application/json",
+//                 "Accept": "*/*",
+//                 "Authorization": "Bearer " + window.localStorage.getItem("accessToken")
+//             }
+//         })
+//         .then(response => response.json());
+
+//         console.log('[AttendanceAPICalls] callAttendanceDetailAPI RESULT : ', result);
+//         if(result.status === 200){
+//             console.log('[AttendanceAPICalls] callAttendanceDetailAPI SUCCESS');
+//             dispatch({ type: GET_STICKER,  payload: result.data });
+//         }
+//     };
+// }
+
+// // 관리자용 근태 등록
+// export const callAttendanceRegistAPI = ({form}) => {
+//     console.log('[AttendanceAPICalls] callAttendanceRegistAPI Call');
+
+//     const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8090/api/v1/attendance-management`;
+
+//     return async (dispatch, getState) => {
+
+//         const result = await fetch(requestURL, {
+//             method: "POST",
+//             headers: {
+//                 "Accept": "*/*",
+//                 "Authorization": "Bearer " + window.localStorage.getItem("accessToken")
+//             },
+//             body: form
+//         })
+//         .then(response => response.json());
+
+//         console.log('[AttendanceAPICalls] callAttendanceRegistAPI RESULT : ', result);
+
+//         dispatch({ type: POST_STICKER,  payload: result });
+        
+//     };    
+// }
+
+// // 관리자용 근태 삭제
+// export const callAttendanceDeleteAPI = ({attendanceCode}) => {
+//     console.log('[AttendanceAPICalls] callAttendanceDeleteAPI Call');
+
+//     const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8090/api/v1/attendance-management/${attendanceCode}`;
+
+//     return async (dispatch, getState) => {
+
+//         const result = await fetch(requestURL, {
+//             method: "DELETE",
+//             headers: {
+//                 "Content-Type": "application/json",
+//                 "Accept": "*/*",
+//                 "Authorization": "Bearer " + window.localStorage.getItem("accessToken")
+//             }
+//         })
+//         .then(response => response.json());
+
+//         console.log('[AttendanceAPICalls] callAttendanceDeleteAPI RESULT : ', result);
+
+//         dispatch({ type: DELETE_STICKER,  payload: result });
+        
+//     };    
+// }
+
+// // 관리자용 근태 수정
+// export const callAttendanceUpdateAPI = ({form}) => {
+//     console.log('[AttendanceAPICalls] callAttendanceUpdateAPI Call');
+    
+//     const attendanceCode = form.get('attendanceCode')     // # formData에 있는 값을 가져올땐 .get메소드로 가져옴
+
+//     const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8090/api/v1/attendance-management/${attendanceCode}`;
+
+//     return async (dispatch, getState) => {
+
+//         const result = await fetch(requestURL, {
+//             method: "PUT",
+//             headers: {
+//                 "Accept": "*/*",
+//                 "Authorization": "Bearer " + window.localStorage.getItem("accessToken")
+//             },
+//             body: form
+//         })
+//         .then(response => response.json());
+
+//         console.log('[AttendanceAPICalls] callAttendanceUpdateAPI RESULT : ', result);
+
+//         dispatch({ type: PUT_STICKER,  payload: result });
+        
+//     };    
+// }
+
+// //카테고리별 근태 목록 조회
+// export const callAttendanceListAboutCategoryAPI = ({categoryCode}) => {
+//     const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8090/api/v1/attendance/categories/${categoryCode}`;
+
+//     return async (dispatch, getState) => {
+
+//         const result = await fetch(requestURL, {
+//             method: "GET",
+//             headers: {
+//                 "Content-Type": "application/json",
+//                 "Accept": "*/*"                
+//             }
+//         })
+//         .then(response => response.json());
+//         if(result.status === 200){
+//             console.log('[AttendanceAPICalls] callAttendanceListAboutCategoryAPI RESULT : ', result);
+//             dispatch({ type: GET_STICKERS,  payload: result.data });
+//         }
+        
+//     };
+// }
+
+// //타입별 근태 목록 조회
+// export const callAttendanceListAboutTypeAPI = ({typeCode}) => {
+//     const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8090/api/v1/attendance/types/${typeCode}`;
+
+//     return async (dispatch, getState) => {
+
+//         const result = await fetch(requestURL, {
+//             method: "GET",
+//             headers: {
+//                 "Content-Type": "application/json",
+//                 "Accept": "*/*"                
+//             }
+//         })
+//         .then(response => response.json());
+//         if(result.status === 200){
+//             console.log('[AttendanceAPICalls] callAttendanceListAboutTypeAPI RESULT : ', result);
+//             dispatch({ type: GET_STICKERS,  payload: result.data });
+//         }
+        
+//     };
+// }
--- src/pages/attendance/Attendance.js
+++ src/pages/attendance/Attendance.js
@@ -1,27 +1,158 @@
 import AttendanceCSS from './Attendance.module.css';
 import { BsClipboard2Minus, BsClipboard2Plus, BsFileEarmarkText, BsCarFront,  BsAirplane } from "react-icons/bs";
 import { NavLink } from 'react-router-dom';
-// import { useEffect, useState } from "react";
-// import { useSelector, useDispatch } from 'react-redux';
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from 'react-redux';
+// import Moment from 'react-moment';
+// import {useInterval} from "use-interval";
 
-function Main() {
+import {
+    callAttendanceDetailAPI,
+    callAttendanceUpdateAPI
+} from '../../apis/AttendanceAPICalls';
+
+function Attendance() {
+    const dispatch = useDispatch();
+    const status  = useSelector(state => state.AttendanceReducer); 
+
+    useEffect(
+        () => {
+            dispatch(callAttendanceDetailAPI({
+                empNo: 1
+            }));
+        } // eslint-disable-next-line
+        ,[]
+    );
+    console.log(status)
+
+    // 현재 날짜용 (년월일요일 등 근무시간 위에)
+    const todayDate = new Date();   //현재 날짜
+
+    let h = todayDate.getHours() < 12 ? "AM" : "PM"; // 구분
+
+    const week = ['일', '월', '화', '수', '목', '금', '토'] // 요일값
+
+    let newTime = {
+        month: String(todayDate.getMonth()+1).padStart(2, "0"), // 월
+        date: String(todayDate.getDate()).padStart(2, "0"), // 일
+        day: week[String(todayDate.getDay()).toUpperCase()], // 요일
+        hours: String(todayDate.getHours()).padStart(2, "0"), // 시간
+        minutes: String(todayDate.getMinutes()).padStart(2, "0"), //현재 분
+    };    
+
+    // 근무시간 비교용
+        // 출근시간 문자열로 받아오는걸 Date형식으로 변환
+    // const attTime = new Date(status.startTime);
+    const attTime = new Date('2023-04-03 17:00:00') // 임시확인용
+
+    let oldTime = {
+        hours: String(attTime.getHours()).padStart(2, "0"), // 시간
+        minutes: String(attTime.getMinutes()).padStart(2, "0"), //현재 분
+        seconds: String(attTime.getSeconds()).padStart(2, "0") //현재 초
+    };
+
+    // console.log(status.startTime);
+    // console.log(`${todayDate.getFullYear()}-${newTime.month}-${newTime.date} ${newTime.hours}:${newTime.minutes}:${todayDate.getSeconds()}`);
+
+    // 출퇴근 시간처리 (최적화g)
+    const [nowTime, setNowTime] = useState("00:00:00");
+    const [intervalId, setIntervalId] = useState(null);
+
+    const onClickStartAttendanceHandler = () => {
+    // if (!intervalId) {
+    //     const id = setInterval(() => {
+    //         const todayTime = new Date();
+    //         const hours = String(todayTime.getHours() - oldTime.hours).padStart(2, "0");        // # pad 안 붙는거 계산식 순서 바꿔서 해결(계산된 값에 pad)
+    //         const minutes = String(todayTime.getMinutes() - oldTime.minutes).padStart(2, "0");
+    //         const seconds = String(todayTime.getSeconds() - oldTime.seconds).padStart(2, "0");
+    //         setNowTime(`${hours}:${minutes}:${seconds}`);
+    //     }, 1000);
+    //     setIntervalId(id);
+    // }
+
+    dispatch(callAttendanceUpdateAPI({          // # 문제발생
+        empNo: 1
+    }));
+
+    };
+
+    const onClickStopAttendanceHandler = () => {
+        clearInterval(intervalId);
+        setIntervalId(null);
+        setNowTime("00:00:00");
+    };
+
+    useEffect(() => {
+    return () => {
+        clearInterval(intervalId);
+    };
+    }, [intervalId]);
+
+
+                // # 문자열로 출근값 뽑고 현재값 문자열로 변환해서 비교 시도 / 결과 : NaN
+                //   const dada = `${todayDate.getFullYear()}-${time.month}-${time.date} ${time.hours}:${time.minutes}:${todayDate.getSeconds()}`
+                //   console.log(dada - status.startTime);
+
+
+                // # 날짜 형태로 비교 시도  / 결과 : 잘 나옴 (결과엔 pad 적용 안 됨)
+                //   const newDate = new Date();
+                //   const oldDate = new Date('2023-04-03 17:00:00')
+
+                //   console.log(newDate);
+                //   console.log(oldDate);
+
+                //   console.log(newDate - oldDate);
+
+                //   console.log((String(newDate.getHours()).padStart(2, "0") - String(oldDate.getHours()).padStart(2, "0")));
+                //   console.log(String(newDate.getMinutes()).padStart(2, "0") - String(oldDate.getMinutes()).padStart(2, "0"));
+                //   console.log(String(newDate.getSeconds()) - String(oldDate.getSeconds()).padStart(2, "0"));
+
+
+                // # toLocaleString메소드를 이용한 사용자 지정 형식
+                //   console.log(todayDate.toLocaleString('ko-KR', {
+                //     weekday: 'short', // long, short, narrow
+                //     day: '2-digit', // numeric, 2-digit
+                //     year: '2-digit', // numeric, 2-digit
+                //     month: 'long', // numeric, 2-digit, long, short, narrow
+                //     hour: 'numeric', // numeric, 2-digit
+                //     minute: 'numeric', // numeric, 2-digit
+                //     second: 'numeric', // numeric, 2-digit
+                // }));
+                // Output: Tue, July 21, 2020, 10:01:14 AM
+
+    // useInterval(()=>{
+    //     setNowTime(Date.now())
+    // },1000)
+                // # pad 대신 10이하일때 0 붙이기
+                // if (status.usedHalf < 10) {
+                //     status.usedHalf = '0' + status.usedHalf
+                // }
+                // if (status.usedDays < 10) {
+                //     status.usedDays = '0' + status.usedDays
+                // }
+                // if (status.leftDays < 10) {
+                //     status.leftDays = '0' + status.leftDays
+                // }    #총 사용 연차에서 문자열 합쳐지는 문제 발생
+
+    // const token = decodeJwt(window.localStorage.getItem("accessToken"));
+
     return (
         <div>
             <div className={AttendanceCSS.contour}> 근태 </div>
 
-            <div style={{height:'800px', border:'1px solid black'}}>
-                <div className={ AttendanceCSS.horizontalDiv } style={{border:'1px solid red'}}>
+            <div style={{height:'800px', minWidth:'1680px'}}>
+                <div className={ AttendanceCSS.horizontalDiv }>
                     <div className={ AttendanceCSS.attendance }>
                         <div>
-                            <span>AM 09:00 03/03(금)</span>
+                            <span>{h} {newTime.hours}:{newTime.minutes} {newTime.month}/{newTime.date}({newTime.day})</span>
                         </div>
                         <br></br>
-                        <span className={ AttendanceCSS.empName }><strong>공성식</strong>님의 근무시간은</span>
+                        <span className={ AttendanceCSS.empName }><strong>{status.empName}</strong>님의 근무시간은</span>
                         <br></br>
-                        <span className={ AttendanceCSS.time }>00:32:59</span>
+                        <span className={ AttendanceCSS.time }>{nowTime}</span>
                         <div className={ AttendanceCSS.btnAtd }>
-                            <button>출근</button>
-                            <button>퇴근</button>
+                            {status.statusName === "출근" ? <button onClick={onClickStartAttendanceHandler} style={{background:'red', color:'white', fontWeight:'bold'}}>출근</button> : <button onClick={onClickStartAttendanceHandler}>출근</button>}
+                            {status.statusName === "퇴근" ? <button onClick={onClickStopAttendanceHandler} style={{background:'red', color:'white', fontWeight:'bold'}}>퇴근</button> : <button onClick={onClickStopAttendanceHandler}>퇴근</button>}
                         </div>
                     </div>
                     <div className={ AttendanceCSS.approvals }>
@@ -67,26 +198,26 @@ function Main() {
                         </div>
                     </div>
                 </div>
-                <div className={ AttendanceCSS.middleContent } style={{border:'1px solid black'}}>
+                <div className={ AttendanceCSS.middleContent }>
                     <div className={ AttendanceCSS.vacation }>
                         <span className={ AttendanceCSS.totalVacation }>- 연차 사용 현황</span>
-                        <span>기본 연차 : 15개</span>
+                        <span>기본 연차 : {status.allDays}개</span>
                         <div className={ AttendanceCSS.vacationDiv }>
                             <div>
                                 <span>사용한 반차</span>
-                                <span>01개</span>
+                                <span>{status.usedHalf}개</span>
                             </div>
                             <div>
                                 <span>사용한 연차</span>
-                                <span>01개</span>
+                                <span>{status.usedDays}개</span>
                             </div>
                             <div>
                                 <span>총 사용 연차</span>
-                                <span>1.5개</span>
+                                <span>{status.usedDays + (status.usedHalf / 2)}개</span>
                             </div>
                             <div>
                                 <span>사용가능한 연차</span>
-                                <span>13.5개</span>
+                                <span>{status.leftDays}개</span>
                             </div>
                             </div>
                     </div>
@@ -98,4 +229,4 @@ function Main() {
     );
 }
 
-export default Main;
+export default Attendance;
