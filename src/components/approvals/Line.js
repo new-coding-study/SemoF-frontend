aff0981ceaaa7c908e230c2071102ce2a0e17bd9
aidenlee <lih9490@gmail.com>
2023-04-04 09:23:41 +0900

    230404 구현 중

--- src/components/approvals/Line.js
+++ src/components/approvals/Line.js
@@ -1,6 +1,10 @@
 import { useNavigate } from "react-router-dom";
-
+import { useDispatch } from "react-redux";
+import { callDeleteLineAPI } from "../../apis/ApprovalAPICalls";
 function Line({line : {lineNo, branchName, approvOrderDTOList}}){
+
+    const nav = useNavigate();
+    const dispatch  = useDispatch();
     return(
         <>
         <div> 다른건 뜨니? </div>
@@ -11,6 +15,14 @@ function Line({line : {lineNo, branchName, approvOrderDTOList}}){
                 {/* <h3>{approvOrderDTOList.empNo}</h3> */}
 
             </div>  
+            <div>
+                <button onClick={()=>{
+                    nav(`semof/edit-line`);
+                }}>수정</button>
+                <button onClick={()=>{
+                    dispatch(callDeleteLineAPI(lineNo));
+                }}>삭제</button>
+            </div>
         </>
     );
 
