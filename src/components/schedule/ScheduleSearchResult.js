40cbcca729d32a2d1e155a4b0633c546dd4fb523
hijee-ce <wlgml6925@gmail.com>
2023-04-12 21:00:35 +0900

    <23.04.12 21:00> - 일정관리 검색결과 시간 포맷 수정 중

--- src/components/schedule/ScheduleSearchResult.js
+++ src/components/schedule/ScheduleSearchResult.js
@@ -1,4 +1,6 @@
 import ScheduleSearchResultCSS from "./ScheduleSearchResult.module.css";
+import moment from "moment";
+// import { moment }
 
 function ScheduleSearchResult({ scheduleSearch }) {
   console.log(scheduleSearch);
@@ -20,18 +22,56 @@ function ScheduleSearchResult({ scheduleSearch }) {
   // scdStartTime: "14:00:00";
   // scdWriter: 41;
   // scdWriterName: null;
+
+  // const test = new Date();
+  // console.log(test.moment.fo);
+
+  const scdDate = moment(scheduleSearch?.scdStartDay).format(
+    "YYYY년 MM월 DD일"
+  );
+
+  const week = ["일", "월", "화", "수", "목", "금", "토"]; // 요일 배열
+
+  const scdDay = week[moment(scheduleSearch?.scdStartDay).day()];
+
+  //   const scdStartTime = moment(new Date(scheduleSearch?.scdStartTime)).format(
+  //     "hh시 mm분"
+  //   );
+  //   const testTiem = new Date(String(scheduleSearch?.scdStartTime));
+  //   console.log("testTime ", testTiem);
+  //   console.log(
+  //     moment(scheduleSearch?.scdStartTime).format("YYYY:MM:dd HH:mm:ss")
+  //   );
+
+  //   console.log(scdStartTime);
+
+  const scdTime = scheduleSearch?.scdStartTime.substr(0, 2);
+  console.log("scdTime", scdTime);
+
+  const condition = scdTime < 12 ? "오전" : "오후";
+
+  const newScdTime = scdTime < 12 ? scdTime : scdTime - 12;
+
+  console.log("condition", condition);
+  console.log("newScdTime", newScdTime);
+  // const timeFormat = scheduleSearch?.scdStartTime.padStart(2, "0")
+
   return (
     <>
       <div className={ScheduleSearchResultCSS.scdResultOneWrapper}>
         <div className={ScheduleSearchResultCSS.scdDate}>
-          {scheduleSearch?.scdStartDay}
+          {/* {scheduleSearch?.scdStartDay} */}
+          {scdDate}
         </div>
-        <div className={ScheduleSearchResultCSS.scdDay}> 요일</div>
+        <div className={ScheduleSearchResultCSS.scdDay}>{scdDay} </div>
         <div
           className={ScheduleSearchResultCSS.calColorWrapper}
-          style={{ backgroundColor: scheduleSearch?.calColor }}
+          style={{ marginTop: "2px" }}
         >
-          {/* <div style={{ backgroundColor: scheduleSearch?.calColor }}></div> */}
+          <div
+            className={ScheduleSearchResultCSS.calColor}
+            style={{ backgroundColor: scheduleSearch?.calColor }}
+          ></div>
         </div>
         <div className={ScheduleSearchResultCSS.scdTime}>
           <span> {scheduleSearch?.scdStartTime} </span>
